b3698c505be5b71887f5f6632b4abf7bf2122bcd
lwz <1055824818@qq.com>
2023-03-06 19:52:14 +0800

    使用dotenv抽离常量

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Koa from "koa"
 import KoaRouter from "@koa/router"
+import { SERVER_PORT } from "./config/server"
 
 const app = new Koa()
 
@@ -11,6 +12,6 @@ useRouter.get("/list", (ctx: Koa.BaseContext, next: Koa.Next) => {
 app.use(useRouter.routes())
 app.use(useRouter.allowedMethods())
 
-app.listen(8000, () => {
+app.listen(SERVER_PORT, () => {
   console.log("coderhub服务器启动成功~")
 })
