4de6b13e0ca0678e3a8929bb4a2ef0cd3e5b732b
lwz <1055824818@qq.com>
2023-03-13 21:59:19 +0800

    feat 抽离 main 文件中内容到对应的文件

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,13 +1,12 @@
 import Koa from "koa"
 import KoaRouter from "@koa/router"
+import userController from "../controller/user.controller"
 
 // 1. 创建路由对象
 const userRouter = new KoaRouter({ prefix: "/users" })
 
 // 2. 定义路由中映射
-userRouter.get("/list", (ctx: Koa.BaseContext, next: Koa.Next) => {
-  ctx.body = `users list`
-})
+userRouter.get("/", userController.create)
 
 // 3. 导出路由
 export { userRouter }
---
+++ src/service/user.service.ts
@@ -0,0 +1,7 @@
+class UserService {
+  create() {
+    console.log("将user对象保存到数据库中")
+  }
+}
+
+export default new UserService()
