f71efea206d8026b364d6e3b1055f591cd21e927
lwz <1055824818@qq.com>
2023-03-20 19:21:39 +0800

    fix 登录接口的边界判断

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,12 +6,32 @@ class UserController {
   async create(ctx: Koa.ExtendableContext, next: Koa.Next) {
     // 1. 获取用户传递过来的信息
     const user = ctx.request.body
-    console.log(user)
 
-    // 2. 将user信息存储到数据库中
+    // 2. 验证客户端传递过来的user是否可以保存在数据库中
+    // 2.1 验证用户名和密码是否为空
+    const { name, password } = user as IUser
+    if (!user || !password) {
+      ctx.body = {
+        code: -1001,
+        message: "用户名或者密码不能为空~"
+      }
+      return
+    }
+
+    // 2.2 判断那么是否在数据库中已经存在
+    const users = await userService.findUserByName(name)
+    if (users.length) {
+      ctx.body = {
+        code: -1002,
+        message: "用户名已经被占用, 不能使用~"
+      }
+      return
+    }
+
+    // 3. 将user信息存储到数据库中
     const result = await userService.create(user as IUser)
 
-    // 3. 查看存储的结果,告知前端创建成功
+    // 4. 查看存储的结果,告知前端创建成功
     ctx.body = {
       message: "创建用户成功~~",
       data: result
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -10,8 +10,14 @@ class UserService {
     const statement = "INSERT INTO `user` (name, password) VALUES (?, ?);"
 
     // 3. 执行sql语句
-    const result = await connection.execute(statement, [name, password])
+    const [result] = await connection.execute(statement, [name, password])
     return result
+  }
+
+  async findUserByName(name: string) {
+    const statement = "SELECT * FROM `user` WHERE name = ?;"
+    const [values] = await connection.execute(statement, [name])
+    return values
   }
 }
 
